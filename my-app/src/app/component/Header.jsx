7db1bec61f73258600afccfb1163d3731946456d
sindhu232003 <genplusdev7@gmail.com>
2025-10-07 12:54:46 +0530

    1st commit

---
+++ my-app/src/app/component/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{ padding: '20px', background: '#333', color: '#fff' }}>
+      <h2>My Project Header</h2>
+    </header>
+  );
+}
---
+++ my-app/src/app/page.jsx
@@ -0,0 +1,9 @@
+// Correct default export
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello Next.js Page</h1>
+      <p>This is working fine now!</p>
+    </div>
+  )
+}
